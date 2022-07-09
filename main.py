8d1ff8db1c693a6243b539fdd8cabf1343b4b6b2
JouEsel <fleday228@gmail.com>
2022-07-09 10:17:14 +0300

    Предпросмотр таблицы в консоли

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import openpyxl
 from datetime import datetime
 from config import Config
+from prettytable import PrettyTable
 
 
 def main():
     config = Config()
     book = openpyxl.Workbook()
     sheet = book.active
+
+    table = PrettyTable(config.first_row)
 
     sheet.append(config.first_row)
     sheet.append(config.second_row)
@@ -20,14 +23,30 @@ def main():
         number_of_proxies = len(proxies)
 
     for i in range(len(cookies)):
-        sheet[i + 3][0].value = i + 1
-        sheet[i + 3][1].value = cookies[i]
+        data = [i + 1, cookies[i]]
         if i < number_of_proxies:
-            sheet[i + 3][2].value = proxy_type
-            sheet[i + 3][3].value = proxies[i]
+            data.append(proxy_type)
+            data.append(proxies[i])
+
+        sheet.append(data)
+        preview_data = [i + 1, str(cookies[i])[:15]]
+        if i < number_of_proxies:
+            preview_data.append(proxy_type)
+            preview_data.append(proxies[i].split(':', 1)[0])
+        else:
+            preview_data.extend(('', ''))
+
+        preview_data.extend(('', ''))
+        table.add_row(preview_data)
+
+    print()
+    print("Получилась следующая таблица:")
+    print(table)
 
     try:
-        book.save(f"Dolphin_profiles_{datetime.now().date()}.xlsx")
+        table_name = f"Dolphin_profiles_{datetime.now().date()}.xlsx"
+        book.save(table_name)
+        print(f"Таблица {table_name} успешно создана!")
     except PermissionError:
         print("Похоже, таблица уже открыта в другой программе. Закройти таблицу и попробуйте снова!")
 
